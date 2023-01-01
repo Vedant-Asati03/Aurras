9c3d025ff5e2d4d664dfb424a00277126c01e083
Vedant Asati <vedant.asati03@gmail.com>
2023-01-01 14:42:52 +0530

    bug fixed

--- mpvsetup.py
+++ mpvsetup.py
@@ -6,7 +6,6 @@ input.conf file
 """
 
 import os
-import sys
 
 
 def mpv_setup():
@@ -17,21 +16,23 @@ def mpv_setup():
     try:
         os.makedirs(os.path.join(os.path.expanduser("~"), "AURRAS", "mpv"))
 
+        path = os.path.join(os.path.expanduser("~"), "AURRAS", "mpv")
+
+        # Creates a mpv.conf file
+        with open(
+            os.path.join(path, "mpv.conf"), "w", encoding="UTF-8"
+        ) as mpv_conf_file:
+
+            mpv_conf_file.write("--really-quiet\n--no-video\nvolume-max=130\n")
+
+        # Creates a input.conf file
+        with open(
+            os.path.join(path, "input.conf"), "w", encoding="UTF-8"
+        ) as mpv_input_file:
+
+            mpv_input_file.write(
+                "UP    add volume 7\nDOWN    add volume -7\nWHEEL_UP   add volume 2\nWHEEL_DOWN    add volume -2\nc    quit\n"
+            )
+
     except FileExistsError:
-        sys.exit()
-
-    path = os.path.join(os.path.expanduser("~"), "AURRAS", "mpv")
-
-    # Creates a mpv.conf file
-    with open(os.path.join(path, "mpv.conf"), "w", encoding="UTF-8") as mpv_conf_file:
-
-        mpv_conf_file.write("--really-quiet\n--no-video\nvolume-max=130\n")
-
-    # Creates a input.conf file
-    with open(
-        os.path.join(path, "input.conf"), "w", encoding="UTF-8"
-    ) as mpv_input_file:
-
-        mpv_input_file.write(
-            "UP    add volume 7\nDOWN    add volume -7\nWHEEL_UP   add volume 2\nWHEEL_DOWN    add volume -2\nc    quit\n"
-        )
+        pass
