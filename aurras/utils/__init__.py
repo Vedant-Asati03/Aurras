5362cc634d1b61ad37da009ff0ce6029771a44e0
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-15 01:11:06 +0530

    Add utility module for Aurras music player with common functions and classes

---
+++ aurras/utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Utility functions and classes for the Aurras music player.
+
+This package contains utility functionality used throughout the application,
+such as path management, decorators, exceptions, and logging.
+"""
