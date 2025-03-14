b8115d8762b939cc176bad8e896c53d18d7d54a9
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-15 01:06:40 +0530

    Add __init__.py for Aurras package core functionality and metadata

---
+++ aurras/__init__.py
@@ -0,0 +1,8 @@
+"""
+Aurras - A command-line music player with online and offline capabilities
+
+This package contains the core functionality and interfaces for the Aurras music player.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Vedant Asati"
