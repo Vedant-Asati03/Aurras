79a776f45f262041e1a47bfa1cced709b489f593
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-15 01:09:25 +0530

    Add module for external service integrations in Aurras music player

---
+++ aurras/services/__init__.py
@@ -0,0 +1,6 @@
+"""
+External service integrations for the Aurras music player.
+
+This package contains modules for interfacing with external services like
+Spotify, YouTube, and other music streaming platforms.
+"""
