179d95dc8ed506cc1dc6c16ee237eb2a716234a1
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-15 01:07:04 +0530

    Add core functionality documentation for Aurras music player

---
+++ aurras/core/__init__.py
@@ -0,0 +1,6 @@
+"""
+Core functionality for the Aurras music player.
+
+This package contains the central components that coordinate the functionality
+of other modules, such as the main player interface and core services.
+"""
