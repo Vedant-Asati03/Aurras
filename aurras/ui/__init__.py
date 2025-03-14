00ecd84aa172330c929fe685ef6d8118853e9516
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-15 01:10:04 +0530

    Add user interface components for Aurras music player

---
+++ aurras/ui/__init__.py
@@ -0,0 +1,6 @@
+"""
+User interface components for the Aurras music player.
+
+This package contains modules for both command-line interface (CLI)
+and text-based user interface (TUI) components.
+"""
