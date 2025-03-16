e0cd7bdf83695c542a6b53b124250fc560ac5c19
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-16 11:40:29 +0530

    Bump version number to 1.1.1 in __init__.py

--- aurras/__init__.py
+++ aurras/__init__.py
@@ -4,5 +4,5 @@ Aurras - A command-line music player with online and offline capabilities
 This package contains the core functionality and interfaces for the Aurras music player.
 """
 
-__version__ = "0.1.0"
+__version__ = "1.1.1"
 __author__ = "Vedant Asati"
