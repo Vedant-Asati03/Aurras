93082588e0b4c12f29e8a0848e3b59475452eb52
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-26 03:21:11 +0530

    Remove PlayerControls from Aurras TUI widget exports

--- aurras/tui/widgets/__init__.py
+++ aurras/tui/widgets/__init__.py
@@ -5,7 +5,6 @@ Custom widgets for the Aurras TUI.
 from .playlist import Playlist
 from .panel.library import LibraryPanel
 from .panel.tracks import TrackPanel
-from .player_controls import PlayerControls
 from .volume_control import VolumeControl
 from .empty import Empty
 
@@ -13,7 +12,6 @@ __all__ = [
     "Playlist",
     "LibraryPanel",
     "TrackPanel",
-    "PlayerControls",
     "VolumeControl",
     "Empty",
 ]
