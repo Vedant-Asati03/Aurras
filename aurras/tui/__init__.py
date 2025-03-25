197faa26635269cb66d6844d476f5d245070382c
vedant-asati03 <vedant.asati03@gmail.com>
2025-03-25 11:10:19 +0530

    Add TUI module for Aurras Music Player using Textual library

---
+++ aurras/tui/__init__.py
@@ -0,0 +1,9 @@
+"""
+Terminal User Interface for Aurras Music Player.
+
+This module contains the TUI implementation using the Textual library.
+"""
+
+from .app import AurrasTUI
+
+__all__ = ["AurrasTUI"]
