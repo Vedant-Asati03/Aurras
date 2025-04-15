618343218648358542e9b1a69771d3657512b47f
vedant-asati03 <vedant.asati03@gmail.com>
2025-04-16 03:50:19 +0530

    Add settings models package for validation and manipulation

---
+++ aurras/core/settings/models/__init__.py
@@ -0,0 +1,22 @@
+"""
+Settings Models Package
+
+This package contains all the Pydantic models used for settings validation and manipulation.
+"""
+
+from .base_settings import Settings
+from .keyboard import KeyboardShortcuts
+from .validators import AVAILABLE_THEMES
+from .appearance import AppearanceSettings
+from .backup import BackupItems, TimedBackup, ManualBackup, Backup
+
+__all__ = [
+    "Settings",
+    "KeyboardShortcuts",
+    "BackupItems",
+    "TimedBackup",
+    "ManualBackup",
+    "Backup",
+    "AppearanceSettings",
+    "AVAILABLE_THEMES",
+]
