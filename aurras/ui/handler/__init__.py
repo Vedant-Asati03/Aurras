27419622b6a518e61d2f1b527991832f639a4636
vedant-asati03 <vedant.asati03@gmail.com>
2025-04-18 11:34:35 +0530

    Renamed the handlers to handler to be more consistent with the architecture

---
+++ aurras/ui/handler/__init__.py
@@ -0,0 +1,17 @@
+"""
+UI Handlers for Aurras Music Player.
+
+This package contains handlers for user input, shortcuts, settings, and keyboard input:
+- input_handler: Main input handler using component-based architecture
+- shorthand_handler: Registration of shorthand commands
+- settings_handler: Management of application settings
+- keyboard_shortcut_handler: Management of keyboard shortcuts and key bindings
+"""
+
+from .command import register_all_commands
+from .shorthand import register_default_shorthands
+
+__all__ = [
+    "register_all_commands",
+    "register_default_shorthands",
+]
