6ddee5045d9342b0a34fa5571aa9c990a89a1bf7
vedant-asati03 <vedant.asati03@gmail.com>
2025-05-13 00:46:33 +0530

    Remove unused docstring from utils module

--- aurras/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Utility functions and classes for the Aurras music player.
-
-This package contains utility functionality used throughout the application,
-such as path management, decorators, exceptions, and logging.
-"""
