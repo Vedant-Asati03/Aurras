aef552e341b0da3f125c9bc435a304b1cc7a8fff
vedant-asati03 <vedant.asati03@gmail.com>
2025-05-13 00:35:39 +0530

    Refactor __init__.py: Update import statement for LyricsManager to use absolute path and remove unused exports

--- aurras/services/lyrics/__init__.py
+++ aurras/services/lyrics/__init__.py
@@ -4,17 +4,8 @@ Lyrics Module Package
 This package provides functionality for fetching synced and plain lyrics for songs.
 """
 
-from .manager import LyricsManager
-from .cache import LyricsCache
-from .fetcher import LyricsFetcher
-from .parser import LyricsParser
-from .formatter import LyricsFormatter
+from aurras.services.lyrics.manager import LyricsManager
 
 __all__ = [
     "LyricsManager",
-    "LYRICS_AVAILABLE",
-    "LyricsCache",
-    "LyricsFetcher",
-    "LyricsParser",
-    "LyricsFormatter",
 ]
