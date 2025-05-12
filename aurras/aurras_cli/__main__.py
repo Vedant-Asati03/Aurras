bda883dc5a38116cc677615e9e24a0606ebd95aa
vedant-asati03 <vedant.asati03@gmail.com>
2025-05-13 00:30:02 +0530

    Fix import statement in main execution module to use absolute path

--- aurras/aurras_cli/__main__.py
+++ aurras/aurras_cli/__main__.py
@@ -4,7 +4,7 @@ Main execution module for Aurras.
 This module allows running Aurras as a module via `python -m aurras`.
 """
 
-from ..utils.command.dispatcher import main
+from aurras.utils.command.dispatcher import main
 
 if __name__ == "__main__":
     main()
