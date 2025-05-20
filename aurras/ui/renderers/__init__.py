74b2e95aa321597c22b01b611d1fa2d90143e732
vedant-asati03 <vedant.asati03@gmail.com>
2025-05-21 02:09:44 +0530

    Refactor: Add UI renderers for Aurras Music Player with logging for initialization

---
+++ aurras/ui/renderers/__init__.py
@@ -0,0 +1,21 @@
+"""
+UI Renderers for Aurras Music Player.
+
+This package contains components that render UI elements to the user:
+- command_palette: Command palette renderer
+- options_palette: Options palette renderer
+"""
+
+from aurras.utils.logger import get_logger
+from aurras.ui.renderers.command_palette import CommandPalette
+from aurras.ui.renderers.options_palette import OptionsMenuRenderer
+
+logger = get_logger("aurras.ui.renderers", log_to_console=False)
+
+command_palette = CommandPalette()
+logger.debug("Command palette initialized.")
+
+options_menu = OptionsMenuRenderer()
+logger.debug("Options menu renderer initialized.")
+
+__all__ = ["command_palette"]
