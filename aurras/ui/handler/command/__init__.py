4d43929e6c2901176226d3d7861307800d076bf2
vedant-asati03 <vedant.asati03@gmail.com>
2025-05-21 23:56:22 +0530

    feat: Register theme and settings commands in command handler

--- aurras/ui/handler/command/__init__.py
+++ aurras/ui/handler/command/__init__.py
@@ -10,10 +10,12 @@ This package contains domain-specific command modules organized by functionality
 """
 
 from aurras.utils.logger import get_logger
+from aurras.ui.handler.command.theme import register_theme_commands
 from aurras.ui.handler.command.player import register_player_commands
 from aurras.ui.handler.command.system import register_system_commands
 from aurras.ui.handler.command.history import register_history_commands
 from aurras.ui.handler.command.spotify import register_spotify_commands
+from aurras.ui.handler.command.settings import register_settings_commands
 from aurras.ui.handler.command.playlist import register_playlist_commands
 
 logger = get_logger("aurras.ui.handler.command", log_to_console=False)
@@ -21,6 +23,8 @@ logger = get_logger("aurras.ui.handler.command", log_to_console=False)
 
 def register_all_commands():
     """Register all commands from all command modules."""
+    register_theme_commands()
+    logger.debug("Theme commands registered")
     register_player_commands()
     logger.debug("Player commands registered")
     register_system_commands()
@@ -29,5 +33,7 @@ def register_all_commands():
     logger.debug("History commands registered")
     register_spotify_commands()
     logger.debug("Spotify commands registered")
+    register_settings_commands()
+    logger.debug("Settings commands registered")
     register_playlist_commands()
     logger.debug("Playlist commands registered")
