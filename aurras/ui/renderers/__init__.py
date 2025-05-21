b63124022706c4dce5d671c9d07bd86451af8058
vedant-asati03 <vedant.asati03@gmail.com>
2025-05-22 02:58:09 +0530

    refactor: Remove options menu renderer and update exports in UI renderers

--- aurras/ui/renderers/__init__.py
+++ aurras/ui/renderers/__init__.py
@@ -8,14 +8,12 @@ This package contains components that render UI elements to the user:
 
 from aurras.utils.logger import get_logger
 from aurras.ui.renderers.command_palette import CommandPalette
-from aurras.ui.renderers.options_palette import OptionsMenuRenderer
+from aurras.ui.renderers.options_palette import options
 
 logger = get_logger("aurras.ui.renderers", log_to_console=False)
 
 command_palette = CommandPalette()
 logger.debug("Command palette initialized.")
 
-options_menu = OptionsMenuRenderer()
-logger.debug("Options menu renderer initialized.")
 
-__all__ = ["command_palette"]
+__all__ = ["command_palette", "options"]
