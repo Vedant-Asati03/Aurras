4fd05728845cc57f798e80cc944937b9cede1ec5
vedant-asati03 <vedant.asati03@gmail.com>
2025-06-03 14:09:47 +0530

    Adds self-processor import and registers version info command in system commands

--- aurras/ui/handler/command/system.py
+++ aurras/ui/handler/command/system.py
@@ -7,7 +7,7 @@ This module contains all system-related commands such as cache management, setti
 from aurras.core.settings import SETTINGS
 from aurras.utils.logger import get_logger
 from aurras.ui.core.registry import command_registry
-from aurras.utils.command.processors import system_processor
+from aurras.utils.command.processors import system_processor, self_processor
 
 logger = get_logger("aurras.ui.handler.command.system", log_to_console=False)
 COMMAND_SETTINGS = SETTINGS.command
@@ -29,6 +29,15 @@ def register_system_commands():
         function=system_processor.cleanup_cache,
         description="Clean up cache",
         parameter_help="<days=30>",
+        requires_args=False,
+        category="System",
+    )
+
+    command_registry.register_command(
+        name="self:version-info",
+        function=self_processor.get_version_info,
+        description="Show Aurras version and installation info",
+        parameter_help=None,
         requires_args=False,
         category="System",
     )
