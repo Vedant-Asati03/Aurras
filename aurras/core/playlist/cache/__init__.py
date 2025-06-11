446c80f6a951ffe7dbbb7460846bfb77222d1ecb
vedant-asati03 <vedant.asati03@gmail.com>
2025-06-12 01:03:42 +0530

    Fix database connection path in playlist cache module

--- aurras/core/playlist/cache/__init__.py
+++ aurras/core/playlist/cache/__init__.py
@@ -14,14 +14,14 @@ from aurras.core.playlist.cache.initialize import InitializePlaylistDatabase
 
 logger = get_logger("aurras.core.playlist.cache", log_to_console=False)
 
-playlist_db_connection = DatabaseConnectionManager(_path_manager.cache_db)
+playlist_db_connection = DatabaseConnectionManager(_path_manager.playlists_db)
 logger.debug(
-    f"Playlist cache database connection initialized: {_path_manager.cache_db}"
+    f"Playlist cache database connection initialized: {_path_manager.playlists_db}"
 )
 
 # Initialize the playlist database when the module is imported
 InitializePlaylistDatabase().initialize_cache(playlist_db_connection.connection)
-logger.debug(f"Playlist cache database initialized: {_path_manager.cache_db}")
+logger.debug(f"Playlist cache database initialized: {_path_manager.playlists_db}")
 
 
 __all__ = [
