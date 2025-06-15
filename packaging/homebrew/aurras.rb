bb1d0a5b124d3b5093da78ca6ee48686c6518ae7
vedant-asati03 <vedant.asati03@gmail.com>
2025-06-15 07:25:42 +0530

    Bump version to 2.0.0 and update package descriptions for clarity

--- packaging/homebrew/aurras.rb
+++ packaging/homebrew/aurras.rb
@@ -1,10 +1,10 @@
 class Aurras < Formula
   include Language::Python::Virtualenv
 
-  desc "High-end command line music player with YouTube and Spotify integration"
+  desc "High-end command line music player"
   homepage "https://github.com/vedant-asati03/Aurras"
-  url "https://files.pythonhosted.org/packages/source/a/aurras/aurras-1.1.1.tar.gz"
-  sha256 "PLACEHOLDER_SHA256_HASH"  # Will be updated when published
+  url "https://files.pythonhosted.org/packages/source/a/aurras/aurras-2.0.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256_HASH"  # Will be updated when v2.0.0 is published
   license "MIT"
 
   depends_on "python@3.12"
